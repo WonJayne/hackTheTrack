5def0b3d01f5919a42cf24d1d3a3385ebf674d23
Thomas Dubach <thomas.dubach@ivt.baug.ethz.ch>
2024-12-13 16:14:20 +0100

    [HEXALY} it runs but is it correct?

--- src/scripts/solving_with_hexaly.py
+++ src/scripts/solving_with_hexaly.py
@@ -1,21 +1,38 @@
+from dataclasses import replace
 from pathlib import Path
 import hexaly.optimizer
+from ugraph import NodeIndex
 
 from hackthetrack.dependencygraph.network import DependencyGraph
 from hackthetrack.displib.load_displib_instance import DisplibInstance
 
+horizon = 5 * 60 * 60
+INFINITE = 1e6
+
 
 def main():
-    displib_instance_path = Path("out/instances/displib_instances_phase1/line1_critical_0.json")
+    displib_instance_path = Path("out/instances/displib_instances_phase1/line2_close_0.json")
     instance = DisplibInstance.from_json(displib_instance_path)
 
     network = DependencyGraph.from_displib_instance(instance)
     objective_nodes = [
         network.node_by_train_id_and_index(obj["train"], obj["operation"]) for obj in instance.objectives
     ]
-    objective_node_indices = [node.index for node in objective_nodes]
+    objective_node_indices = [network.node_index_by_name(node.id) for node in objective_nodes]
 
-    num_operations = len(network.all_nodes)
+    num_nodes = len(network.all_nodes)
+
+    node_index_to_train = []
+    release_times_by_resource: dict[str, list[float]] = {}
+
+    for i, node in enumerate(network.all_nodes):
+        node_index_to_train.append(node.train_id)
+
+        for resource in node.resources:
+            if resource.name not in release_times_by_resource:
+                release_times_by_resource[resource.name] = [INFINITE for _ in range(network.n_count)]
+
+            release_times_by_resource[resource.name][i] = resource.release_time
 
     with hexaly.optimizer.HexalyOptimizer() as optimizer:
         model = optimizer.get_model()
@@ -23,42 +40,54 @@ def main():
         operations = [model.interval(node.start_lb if node.start_lb else 0, horizon) for node in network.all_nodes]
         operation_array = model.array(operations)
 
-        resource_allocation = model.array([model.list(num_operations) for _ in range(num_resources)])
+        resource_allocation = model.array([model.list(num_nodes) for _ in range(len(release_times_by_resource))])
 
-        operation_to_train_array = model.array(operation_to_train)
+        operation_to_train_array = model.array(node_index_to_train)
 
         # lower bound on operation duration and upper bound on start time if applicable
-        for i, operation in enumerate(operations):
-            model.add_constraint(model.length(operation) >= network.all_nodes[i].min_duration)
-            if network.all_nodes[i].start_ub is not None:
-                model.add_constraint(model.start(operation) <= network.all_nodes[i].start_ub)
+        for operation, node in zip(operations, network.all_nodes):
+            model.add_constraint(model.length(operation) >= node.min_duration)
+            if node.start_ub is not None:
+                model.add_constraint(model.start(operation) <= node.start_ub)
 
         # ordering of operations on the same resource with release times if subsequent operations are on different trains
-        for r in range(num_resources):
+        for r, release_times_for_resource in enumerate(release_times_by_resource.values()):
             sequence = resource_allocation[r]
-            release_times_for_resource = model.array(release_times[r])
+            release_times_for_resource_array = model.array(release_times_for_resource)
             sequence_lambda = model.lambda_function(
                 lambda i: operation_array[sequence[i]]
-                + (operation_to_train_array[sequence[i]] != operation_to_train_array[sequence[i + 1]])
-                * release_times_for_resource[sequence[i]]
+                # + (operation_to_train_array[sequence[i]] != operation_to_train_array[sequence[i + 1]])
+                # * release_times_for_resource_array[sequence[i]]
                 < operation_array[sequence[i + 1]]
             )
             model.constraint(model.and_(model.range(0, model.count(sequence) - 1), sequence_lambda))
 
         # no-wait and routing of trains
-        for node in network.all_nodes:
+        # ? where is it that a certain train has to use a certain resource? i.e. why can't the resource allocation
+        # ? not just be empty?
+        for i, node in enumerate(network.all_nodes):
+
+            # only do this for active nodes? definitely not good for an objective which could be inactive
+            # maybe we could just do this for all nodes in the sequence and the starting node?
+            resources = [r.name for r in node.resources]
+            for resource in resources:
+                resource_index = list(release_times_by_resource.keys()).index(resource)
+                model.add_constraint(model.contains(resource_allocation[resource_index], i))
+
             neighbors = network.neighbors(node.id, "out")
             if len(neighbors) == 0:
                 continue
             elif len(neighbors) == 1:
                 model.add_constraint(
-                    model.end(operation_array[node.index]) == model.start(operation_array[neighbors[0].index])
+                    model.end(operation_array[i])
+                    == model.start(operation_array[network.node_index_by_name(neighbors[0].id)])
                 )
             else:
                 model.add_constraint(
                     model.or_(
                         [
-                            model.end(operation_array[node.index]) == model.start(operation_array[neighbor.index])
+                            model.end(operation_array[i])
+                            == model.start(operation_array[network.node_index_by_name(neighbor.id)])
                             for neighbor in neighbors
                         ]
                     )
@@ -67,10 +96,22 @@ def main():
         # activate all trains
 
         # objective
-        model.minimize(model.sum(model.start(operation_array[i]) for i in objective_node_indices))
+        model.minimize(model.sum([model.start(operation_array[i]) for i in objective_node_indices]))
         model.close()
 
+        # Parameterize the optimizer
+        optimizer.param.time_limit = 60 * 10
+
         optimizer.solve()
+
+        operation_solution = operation_array.value
+        for i, node in enumerate(network.all_nodes):
+            network.replace_node(
+                NodeIndex(i),
+                replace(node, start_lb=operation_solution[i].start(), start_ub=operation_solution[i].start()),
+            )
+
+        ...
 
 
 if __name__ == "__main__":
